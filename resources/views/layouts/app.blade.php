d1568e9fabc74bb05f9039c23a45074ef942d969
Hannah Tinkler <hannah.tinkler@e3creative.co.uk>
2019-09-10 09:52:31 +0100

    Updates favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,6 @@
 
     <title>{{ config('app.name') }}</title>
 
-    <!-- Custom styles for this template -->
     <link href="/css/app.css" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
   </head>
